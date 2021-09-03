4963669690e87226438d0b45c5f5deeab0413d5e
Diogo Izele <diogogabrielizele@gmail.com>
2021-09-03 19:35:16 -0300

    insertion of lines in the initial banner and layout of (about section) partially finished

--- src/assets/js/arrowFilter.js
+++ src/assets/js/arrowFilter.js
@@ -1,10 +1,8 @@
 setInterval(() => {
   const arrow = document.querySelector(".mobile-arrow");
-  arrow.style.filter =
-    "invert(98%) sepia(100%) saturate(3007%) hue-rotate(340deg) brightness(95%) contrast(80%) drop-shadow(0 0 1rem #ffcf1a)";
+  arrow.style.filter = "drop-shadow(0 0 1rem #ffcf1a)";
 }, 1000 * 2);
 setInterval(() => {
   const arrow = document.querySelector(".mobile-arrow");
-  arrow.style.filter =
-    "invert(98%) sepia(100%) saturate(3007%) hue-rotate(340deg) brightness(95%) contrast(80%)";
+  arrow.style.filter = "none";
 }, 2000 * 2);
