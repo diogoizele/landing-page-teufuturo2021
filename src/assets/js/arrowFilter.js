fbec6543ea3ac107777e215768db144d72421acc
Diogo Izele <diogogabrielizele@gmail.com>
2021-09-10 15:35:15 -0300

    landing page form finished

--- src/assets/js/arrowFilter.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-  const arrow = document.querySelector(".mobile-arrow");
-  arrow.style.filter = "drop-shadow(0 0 1rem #ffcf1a)";
-}, 1000 * 2);
-setInterval(() => {
-  const arrow = document.querySelector(".mobile-arrow");
-  arrow.style.filter = "none";
-}, 2000 * 2);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,37 +4,37 @@ const data = {
       name: "Diogo Ribeiro",
       testimonial:
         '"Olá me chamo Diogo, tenho 18, sou de Porto Alegre e estou terminando o ensino médio. Estou no momento achando esse curso muito bom, antes de eu entrar nele eu estava bem perdido em como começar e por onde na programação, agora ando me interessando bem mais pela área, sinto que o #teufuturo me deu uma baita oportunidade e com isso pretendo aproveitar ela bastante pois o curso é bom e os professores sempre estão disponíveis pra ajudar a gente com algo. Sempre fui interessado por essa área pois sempre tive vontade de criar um jogo, só pra fazê-lo com todos os gêneros e mecânicas específicas de outros jogos, só descobri que não iria ser tão fácil assim de começo de desenvolver e apenas estudando mais essa área que iria ter uma boa noção de como começar."',
-      email: "diogo.ribeiro3006@gmail.com",
+      linkedin: "https://www.linkedin.com/in/diogooribeiroo",
     },
     {
       name: "Carolina da Silva",
       testimonial:
         '"Meu nome é Carolina da Silva, tenho 18 anos e sou residente de Porto Alegre. O curso do #TeuFuturo, tem sido de extrema importância na minha vida, sempre gostei de tecnologia, inteligência artificial e por meio de um curso que fiz em 2019 de robótica educacional acabei criando ainda mais interesse na área."',
-      email: "dasilvacarolina.carolina@gmail.com",
+      linkedin: "https://www.linkedin.com/in/carolinadasilva5/",
     },
     {
       name: "Diogo Izele",
       testimonial:
         '"Olá, me chamo Diogo Izele, tenho 19 anos e moro em Marau-RS. Iniciei no mundo da programação em 2019, ao ingressar no Curso Técnico Integrado de Informática pelo IFSUL – Campus Passo Fundo. \n Durante minha trajetória na área me deparei com diversos desafios que todo iniciante em programação enfrenta, porém com dedicação e prática consegui entender como o desenvolvimento web funciona e atualmente sinto prazer em desenvolver soulçoes para o front-end com HTML5, CSS3 e JavaScript. \n Eu iniciei no teu futuro por 2 motivos, o primeiro foi me conectar com pessoas que tinham interesses semelhantes aos meus e o segundo foi aprimorar as qualificações comportamentais que um profissional de tecnologia precisa, e acredito que estou aproveitando muito bem essa oportunidade. Estou sempre à disposição para trocar uma ideia, ajudar quem precisa e aprender coisas novas pois é em conjunto que todos crescemos!"',
-      email: "diogogabrielizele@gmail.com",
+      linkedin: "https://www.linkedin.com/in/diogoizele/",
     },
     {
       name: "Cristina Pires da Silveira",
       testimonial:
         '"Meu nome é Cristina Pires da Silveira, tenho 16 anos, sou natural de Porto Alegre e apaixonada por design, arte e cultura, além da incrível tecnologia existente! Meu objetivo de vida sempre foi adquirir conhecimento e aprendizado, crescer como pessoa e profissional, mas precisava de, pelo menos, uma oportunidade para desempenhar isto! Quando soube do projeto #TeuFuturo, não pensei duas vezes e me inscrevi, pois era disso que mais precisava nesse momento de desenvolvimento na minha vida. Tenho vivido momentos incríveis no projeto, aprendido coisas que não compreendia e nem sabia da existência! No programa, desenvolvo habilidades intelectuais, físicas, técnicas e comportamentais, o que será de grande importância e relevância para acrescentar em minha trajetória pessoal e profissional."',
-      email: "cristinapiresdasilveira@gmail.com",
+      linkedin: "https://www.linkedin.com/in/crislink2116",
     },
     {
       name: "Dylan",
       testimonial:
         '"Meu nome é Dylan, tenho 17 anos e sou de porto alegre. \n Meu pai tinha um computador e aos 5 anos, quando aprendi a ler, já comecei a usa-lo e hoje já tenho 12 anos de prática utilizando computadores, aprendi a usar diversos programas, joguei vários jogos, visitei muitos sites, mas em um momento do ano passado (2020) percebi que havia uma coisa que eu não tinha explorado ainda: Como criam todas essas coisas que eu uso a mais de uma década? \n Então depois de pensar sobre isso cogitei a possibilidade de um dia estudar como os jogos, programas e sites eram criados, então quando recebi o e-mail falando sobre o curso eu aceitei o desafio e me inscrevi mesmo sem saber absolutamente nada sobre programação, apenas pela diversão. \n Quando o curso começou não sabia o que esperar, era tudo muito novo pra mim, mas como tenho uma boa estrutura (bom computador e boa internet) não tive nenhuma dificuldade para iniciar a assistir as aulas e me empenhei desde o primeiro dia. Cometi alguns erros até entender como o sistema funcionava, mas logo com a minha experiência com computadores e persistência comecei a entender um pouco da programação, notei que não é algo simples, mas como tudo na vida, devemos nos esforçar para conseguir aprender. \n Até agora estou achando o curso muito interessante, já sou capaz de fazer alguns programas, jogos e sites simples, com muito estudo e autodidatismo, mas consigo. O curso é uma oportunidade muito boa na minha visão, é uma área que falta mão de obra no Brasil, com uma boa remuneração e amplas possibilidades de emprego, toda empresa precisa de um site, máquinas precisam de um software para comanda-las, inteligências artificiais precisam primeiro que uma pessoa a programe para cria-la, e como o mundo tende a se modernizar cada vez mais. A programação é a área dessa década e das próximas, talvez até do século XXI, e se você for uma pessoa interessada e empenhada eu recomendo fortemente que de uma chance ao curso, garanto que você mesmo que não ache à programação a profissão que você queira no futuro ainda sim aprenderá muito e conhecerá pessoas incríveis que serão suas colegas. \n “Permita-se ao conhecimento! Nunca é demais saber a mais”"',
-      email: "dylanfigueiredo@outlook.com",
+      linkedin: "https://www.linkedin.com/in/dylanfigueiredo/",
     },
     {
       name: "Ex-aluna: Isaura Koch",
       testimonial:
         '"Eu participei do #TeuFuturo no ano de 2018, e durante o programa, muito além do técnico pude desenvolver habilidades humanas: comunicação, empatia, liderança, tudo isso em conjunto com a imersão no dia a dia do profissional de TI, que o #TeuFuturo possibilitou, através de visitas a empresas, eventos acadêmicos e projetos práticos. Todos estes aspectos foram essenciais para meu desenvolvimento profissional. Além disso, foi em um evento disponibilizado pelo #TeuFuturo com a IMED e empresas parceiras, que tive meu primeiro contato com o mercado de trabalho, e, posteriormente iniciei minha carreira em uma das empresas participantes do evento. Também foi graças ao programa que consegui ingressar na graduação através de uma bolsa parcial."',
-      email: "koch.isaura@gmail.com",
+      linkedin: "https://www.linkedin.com/in/isaura-koch-a3a990169/",
     },
   ],
 };
@@ -44,14 +44,15 @@ function showInfo(index) {
   const modal = document.createElement("div");
   const name = document.createElement("h4");
   const testimonial = document.createElement("p");
-  const email = document.createElement("p");
+  const linkedin = document.createElement("a");
   const closeButton = document.createElement("button");
 
   data.participants.forEach((participant, idx) => {
     if (index === idx) {
       name.innerHTML = participant.name;
       testimonial.innerHTML = participant.testimonial;
-      email.innerHTML = `contato: ${participant.email}`;
+      linkedin.innerHTML = `LinkedIn: ${participant.name}`;
+      linkedin.setAttribute("href", participant.linkedin);
       closeButton.innerHTML = "✕";
     }
   });
@@ -65,7 +66,7 @@ function showInfo(index) {
   modal.appendChild(closeButton);
   modal.appendChild(name);
   modal.appendChild(testimonial);
-  modal.appendChild(email);
+  modal.appendChild(linkedin);
 
   container.appendChild(modal);
 }
@@ -101,5 +102,44 @@ function sliderImages() {
   }, 4000);
 }
 
+function arrowShadow() {
+  setInterval(() => {
+    const arrow = document.querySelector(".mobile-arrow");
+    arrow.style.filter = "drop-shadow(0 0 1rem #ffcf1a)";
+  }, 1000 * 2);
+  setInterval(() => {
+    const arrow = document.querySelector(".mobile-arrow");
+    arrow.style.filter = "none";
+  }, 2000 * 2);
+}
+
+function submitForm() {
+  const form = document.querySelector("#contactForm");
+  const name = document.querySelector("#inputName");
+  const email = document.querySelector("#inputEmail");
+  const whatsapp = document.querySelector("#inputWhatsapp");
+  const message = document.createElement("p");
+  const obj = {
+    name: name.value,
+    email: email.value,
+    whatsapp: whatsapp.value,
+  };
+
+  message.innerHTML = `Obrigado por se inscrever ${name.value}!`;
+  setTimeout(() => {
+    form.appendChild(message);
+  }, 1000);
+  setTimeout(() => {
+    form.removeChild(message);
+  }, 4000);
+
+  document.querySelector("#inputName").value = "";
+  document.querySelector("#inputEmail").value = "";
+  document.querySelector("#inputWhatsapp").value = "";
+
+  return obj;
+}
+
 animationNavBar();
 sliderImages();
+arrowShadow();
