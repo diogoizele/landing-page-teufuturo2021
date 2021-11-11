a238af46dee69bda5af53125df7270ee31c55a2f
Diogo Izele <diogogabrielizele@gmail.com>
2021-11-11 00:58:38 -0300

    Banner waves fixed import

--- src/components/Banner/BannerWaves.js
+++ src/components/Banner/BannerWaves.js
@@ -1,13 +1,26 @@
 import classes from "./BannerWaves.module.css";
 import React from "react";
+import waveImage from "../../assets/img/wave.webp";
 
 function BannerWaves(props) {
   return (
     <section className={`${props.className} ${classes.waves}`}>
-      <div className={`${classes.wave} ${classes.wave1}`}></div>
-      <div className={`${classes.wave} ${classes.wave2}`}></div>
-      <div className={`${classes.wave} ${classes.wave3}`}></div>
-      <div className={`${classes.wave} ${classes.wave4}`}></div>
+      <div
+        style={{ backgroundImage: `url(${waveImage})` }}
+        className={`${classes.wave} ${classes.wave1}`}
+      ></div>
+      <div
+        style={{ backgroundImage: `url(${waveImage})` }}
+        className={`${classes.wave} ${classes.wave2}`}
+      ></div>
+      <div
+        style={{ backgroundImage: `url(${waveImage})` }}
+        className={`${classes.wave} ${classes.wave3}`}
+      ></div>
+      <div
+        style={{ backgroundImage: `url(${waveImage})` }}
+        className={`${classes.wave} ${classes.wave4}`}
+      ></div>
     </section>
   );
 }
